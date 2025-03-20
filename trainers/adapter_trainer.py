9d420e44d35f0be09497484d54a9293c7b714f01
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-20 16:50:52 +0800

    Update adapter

--- trainers/adapter_trainer.py
+++ trainers/adapter_trainer.py
@@ -8,27 +8,31 @@ class Adapter(nn.Module):
     def __init__(self, num_classes, device):
         super(Adapter, self).__init__()
         self.num_classes = num_classes
-        self.fc = nn.Linear(in_features=num_classes, out_features=num_classes, bias=True, device=device)
+        self.adapter_net = nn.Sequential(nn.Linear(in_features=num_classes, out_features=128, bias=True, device=device),
+                                        nn.ReLU(),
+                                        nn.Linear(in_features=128, out_features=num_classes, bias=True, device=device))
 
     def forward(self, logits):
         prob = torch.softmax(logits, dim=-1)
         U = torch.triu(torch.ones(self.num_classes, self.num_classes).to(logits.device))
         sorted_prob, sorted_indices = torch.sort(prob, descending=True)
+
         #  Caluculate sqrt(r_i - r_(i+1))
         diffs = torch.sqrt(sorted_prob[:, :-1] - sorted_prob[:, 1:])
         diffs = torch.cat((diffs, torch.ones((diffs.shape[0], 1),
                                              dtype=diffs.dtype,
                                              device=diffs.device)), dim=1)
 
+        calibrated_logits = self.adapter_net(prob)
+        calibrated_logits[:, :-1] = torch.sigmoid(calibrated_logits[:, :-1])
+        calibrated_logits = diffs * calibrated_logits
+
         fitted_logits = torch.zeros_like(logits)
         for i in range(logits.shape[0]):
             sorted_row_prob, row_sorted_indices = sorted_prob[i], sorted_indices[i]
             S = torch.zeros(size=(self.num_classes, self.num_classes), device=logits.device)
             S[torch.arange(logits.shape[1]), row_sorted_indices] = 1
-            raw_logits = self.fc(prob[i])
-            raw_logits[:-1] = torch.sigmoid(raw_logits[:-1])
-            raw_logits = diffs[i] * raw_logits
-            fitted_logits[i] = S.T.to(torch.float) @ U @ raw_logits
+            fitted_logits[i] = S.T.to(torch.float) @ U @ calibrated_logits[i]
         fitted_logits = fitted_logits
         return fitted_logits
 
