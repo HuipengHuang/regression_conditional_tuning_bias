7980fad92cc93be7ad990d9705ff39cd5992bbdb
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-21 22:48:53 +0800

    update

--- common/utils.py
+++ common/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import torch
 import numpy as np
 import random
@@ -69,20 +71,33 @@ def split_dataloader(original_dataloader, split_ratio=0.5):
         return subset1, subset2
 
 
+
 def save_exp_result(args, trainer, result_dict, path=None):
+    current_time = datetime.datetime.now()
+    month_day = current_time.strftime("%b%d")
     if path is None:
         path = f"./experiment/{args.algorithm}"
     name = f"{args.dataset}_{args.model}_{args.loss}loss"
-    save_path = os.path.join(path, name)
-    os.makedirs(save_path, exist_ok=True)
-    with open(os.path.join(save_path, "result.txt"), "w") as f:
-        f.write('Epoch, Coverage, Top1 Accuracy, Average Size\n')
 
-        f.write("Detailed Setup \n")
+    save_path = os.path.join(path, month_day)
+    save_path = os.path.join(save_path, args.score)
+
+    i = 0
+    while True:
+        if os.path.exists(os.path.join(save_path, f"{name}_{i}")):
+            i += 1
+        else:
+            break
+    folder_path = os.path.join(save_path, f"{name}_{i}")
+    os.makedirs(folder_path, exist_ok=True)
+    with open(os.path.join(folder_path, "result.txt"), "w") as f:
+        for key in result_dict.keys():
+            f.write(f"{key}: {result_dict[key]}\n")
+
+        f.write("\nDetailed Setup \n")
+
         args_dict = vars(args)
         for k, v in args_dict.items():
             if v is not None:
                 f.write(f"{k}: {v}\n")
-    torch.save(trainer.net.state_dict(), os.path.join(save_path, "_model.pth"))
-    if args.adapter == "True":
-        torch.save(trainer.adapter.adapter_net.state_dict(), os.path.join(save_path, "_adapter.pth"))
+
