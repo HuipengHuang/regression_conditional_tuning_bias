7ec87620ec9b7589b9758dfe9a814d8d15cf08b3
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-22 17:38:10 +0800

    update

--- trainers/weighted_trainer.py
+++ trainers/weighted_trainer.py
@@ -56,7 +56,7 @@ class WeightedTrainer:
         data = data.to(self.device)
         target = target.to(self.device)
         logits = self.net(data)
-        loss = nn.CrossEntropyLoss(logits, target)
+        loss = nn.CrossEntropyLoss()(logits, target)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
