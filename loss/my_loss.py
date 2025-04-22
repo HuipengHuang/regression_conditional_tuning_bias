483d9676d9b268349d24a470fed5f482b16f5e22
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-22 21:41:13 +0800

    update

--- loss/my_loss.py
+++ loss/my_loss.py
@@ -63,6 +63,6 @@ class MyAdapterLoss():
         score = self.predictor.score_function(weight, prob)
         target_score = torch.gather(score, dim=1, index=target.unsqueeze(1))
         loss = torch.sigmoid((target_score.unsqueeze(0) - score) / self.T)
-        print(loss)
         loss = loss.mean()
+        print(loss)
         return loss
