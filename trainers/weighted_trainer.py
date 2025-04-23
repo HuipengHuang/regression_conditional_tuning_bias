58b50cff8cd8d954d3a85a8484f01f9b813e2144
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-23 14:34:05 +0800

    update

--- trainers/weighted_trainer.py
+++ trainers/weighted_trainer.py
@@ -119,7 +119,7 @@ class WeightedTrainer:
         for param in self.first_net.parameters():
             param.requires_grad = False
         if self.args.dataset == "imagenet":
-            epoch = 10
+            epoch = 5
         else:
             epoch = 100
         for i in range(epoch):
