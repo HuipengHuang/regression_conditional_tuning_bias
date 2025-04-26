fd098a7e4b0f22f03bcb69d32e0c7c0c7c76d0dc
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-26 17:43:52 +0800

    update

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ for score in ["thr", "thr"]:
     args.saps_size_penalty_weight = 1
     args.raps_size_penalty_weight = 1
     args.raps_size_regularization = 0
-    trainer.predictor = predictor.Predictor(args, trainer.net)
+
     trainer.predictor.calibrate(cal_loader)
     sub_result_dict = trainer.predictor.evaluate(test_loader)
     print("-----")
