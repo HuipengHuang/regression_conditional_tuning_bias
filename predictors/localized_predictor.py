8fe9e1bc76922c0ec3482131b4082453702e716a
AIAI333 <12312107@mail.sustech.edu.cn>
2025-05-02 20:17:51 +0800

    update

--- predictors/localized_predictor.py
+++ predictors/localized_predictor.py
@@ -87,9 +87,9 @@ class LocalizedPredictor:
         n = self.cal_score.shape[0]
 
         # Vectorized computations
-        Q_diag = torch.diagonal(self.Q, offset=-1)[:n + 1]  # Q[i,i-1] for i=1..n+1
-        Q_rowsum = self.Q[:n + 1, n]  # Q[i,n] for i=1..n+1
-        H_lastcol = self.H[:n + 1, n + 1]  # H[i,n+1] for i=1..n+1
+        Q_diag = torch.diagonal(self.Q, offset=-1)[1 : n+2]  # Q[i,i-1] for i=1..n+1
+        Q_rowsum = self.Q[1:n+2, n]  # Q[i,n] for i=1..n+1
+        H_lastcol = self.H[1:n+2, n + 1]  # H[i,n+1] for i=1..n+1
 
         theta_p = (Q_diag + H_lastcol) / (Q_rowsum + H_lastcol)
         theta = Q_diag / (Q_rowsum + H_lastcol)
