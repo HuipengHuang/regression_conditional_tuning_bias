4e9b00b987e9365879687e9f9046355dfd6cf165
AIAI333 <12312107@mail.sustech.edu.cn>
2025-05-02 14:37:49 +0800

    update

--- common/utils.py
+++ common/utils.py
@@ -46,7 +46,6 @@ def build_dataset(args):
         train_dataset = CIFAR100(root="/data/dataset", download=False, train=True, transform=train_transform)
         cal_test_dataset = CIFAR100(root='/data/dataset', download=False, train=False,
                                  transform=val_transform)
-        cal_test_dataset = Subset(cal_test_dataset, indices=range(200))
 
 
     elif dataset_name == "imagenet":
