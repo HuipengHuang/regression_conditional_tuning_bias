d3e823e15a57c94eb805a6e24e2fd960dd15f3f0
AIAI333 <12312107@mail.sustech.edu.cn>
2025-05-02 13:40:42 +0800

    update

--- models/resnet50.py
+++ models/resnet50.py
@@ -6,10 +6,7 @@ import torchvision.models as models
 class ResNet50(nn.Module):
     def __init__(self, num_classes=1000, pretrained=True):
         super(ResNet50, self).__init__()
-        # Load pretrained ResNet50
-        self.resnet = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-
-        # Replace the final fc layer if you want custom classes
+        self.resnet = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1 if pretrained else None)
         if num_classes != 1000:
             self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
