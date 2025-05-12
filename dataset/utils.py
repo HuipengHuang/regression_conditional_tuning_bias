5a2f425c091ba08a033fb2fd2b9ef12371c3c7fb
AIAI333 <12312107@mail.sustech.edu.cn>
2025-05-12 22:54:09 +0800

    update

--- dataset/utils.py
+++ dataset/utils.py
@@ -23,7 +23,8 @@ def build_dataset(args):
             if args.train_imb == "True":
                 train_dataset = IMBALANCECIFAR10(root='./data/dataset', train=True, transform=train_transform, download=True)
             else:
-                train_dataset = CIFAR100(root='./data/dataset', train=True, transform=train_transform, download=True)
+                train_dataset = CIFAR10(root='./data/dataset', train=True, transform=train_transform, download=True)
+
             cal_test_dataset = IMBALANCECIFAR10(root='./data/dataset', train=False, transform=val_transform, download=True)
             num_class = 10
         elif dataset_name == "cifar100":
@@ -38,7 +39,12 @@ def build_dataset(args):
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             ])
-            train_dataset = IMBALANCECIFAR100(root='./data/dataset', train=True, transform=train_transform)
+            if args.train_imb == "True":
+                train_dataset = IMBALANCECIFAR100(root='./data/dataset', train=True, transform=train_transform,
+                                                 download=True)
+            else:
+                train_dataset = CIFAR100(root='./data/dataset', train=True, transform=train_transform, download=True)
+
             cal_test_dataset = IMBALANCECIFAR100(root='./data/dataset', train=False, transform=val_transform)
             num_class = 100
     elif dataset_name == "cifar10":
